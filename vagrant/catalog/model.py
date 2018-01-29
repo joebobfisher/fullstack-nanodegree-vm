fd2472b2af829c621adc868c553dc4daafdaf557
Joe Fisher <joe.fisher@distek.com>
2018-01-28 21:36:00 -0600

    Basics work!

--- vagrant/catalog/model.py
+++ vagrant/catalog/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
